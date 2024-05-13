85e6dba740c9a321875dcae62953bbe32296c777
Murthy Karra <karrad@karra.co>
2024-05-12 22:37:33 -0700

    Updated vpc.tf to get rid of deprecation warning

--- 1-Infrastructure/vpc.tf
+++ 1-Infrastructure/vpc.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public-subnet-1" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "Public-Subnet-1"
   }
 }
 
@@ -32,7 +32,7 @@ resource "aws_subnet" "public-subnet-2" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "public-subnet-2"
+    Name = "Public-Subnet-2"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_subnet" "public-subnet-3" {
   availability_zone = "us-east-1c"
 
   tags = {
-    Name = "public-subnet-3"
+    Name = "Public-Subnet-3"
   }
 }
 
@@ -52,7 +52,7 @@ resource "aws_subnet" "private-subnet-1" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "private-subnet-1"
+    Name = "Private-Subnet-1"
   }
 }
 
@@ -62,7 +62,7 @@ resource "aws_subnet" "private-subnet-2" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "private-subnet-2"
+    Name = "Private-Subnet-2"
   }
 }
 resource "aws_subnet" "private-subnet-3" {
@@ -71,6 +71,93 @@ resource "aws_subnet" "private-subnet-3" {
   availability_zone = "us-east-1c"
 
   tags = {
-    Name = "private-subnet-3"
+    Name = "Private-Subnet-3"
   }
 }
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+  tags = {
+    Name = "Private-Route-Table"
+  }
+}
+
+
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+resource "aws_route_table_association" "public-subnet-3-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-3.id
+}
+
+
+
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-3.id
+}
+
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  domain                    = "vpc"
+  associate_with_private_ip = "10.0.0.5"
+  tags = {
+    Name = "Production-EIP"
+  }
+}
+
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "Production-NAT-GW"
+  }
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id = aws_route_table.private-route-table.id
+  nat_gateway_id = aws_nat_gateway.nat-gw.id
+
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = aws_vpc.production-vpc.id
+  tags = {
+    Name = "Production-IGW"
+  }
+}
+
+resource "aws_route" "public-internet-gw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
